53a0f51ec460d74b5c7a6d6750d5088eee38db45
ntno <organick.natan@gmail.com>
2019-11-10 22:03:27 -0500

    formatting

--- animations/2.ts
+++ animations/2.ts
@@ -6,22 +6,27 @@ function cycleRainbow () {
     }
     updateBrightness()
 }
+
 function updateBrightness () {
     brightness = (brightness + 1) % 255
     if (brightness <= 100) {
         brightness = brightness + 100
     }
 }
+
 function updateHue () {
     hue = (hue + 1) % 255
 }
+
 let brightness = 0
 let hue = 0
 hue = 255
 brightness = 100
+
 for (let index = 0; index <= 9; index++) {
     light.defaultStrip().setPixelColor(index, light.hsv(hue, 255, brightness))
 }
+
 forever(function () {
     cycleRainbow()
 })
