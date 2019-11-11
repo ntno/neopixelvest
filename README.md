b98b47d1b98d85dd2de1fa88e07cb644f0e5fa35
ntno <organick.natan@gmail.com>
2019-11-10 22:26:18 -0500

    vest animation

--- README.md
+++ README.md
@@ -17,4 +17,4 @@ neopixel animations in makecode
 | [1](https://github.com/ntno/neopixelvest/blob/master/animations/1.ts) | two red lights start at center, move outward, return to center, then single flash at outside before starting over | vertical cylon starting at top |
 | [2](https://github.com/ntno/neopixelvest/blob/master/animations/2.ts) | no animation | rainbow cycle with updating brightness level|
 | [3](https://github.com/ntno/neopixelvest/blob/master/animations/3.ts) | rainbow cycle with updating brightness level | rainbow cycle with updating brightness level|
-
+| [4](https://github.com/ntno/neopixelvest/blob/master/animations/4.ts) | ranbow from 3 or two red lights starting at center, moving outward |  rainbow from 3 or vertical cylon |
---
+++ animations/4.ts
@@ -0,0 +1,113 @@
+function cylon(pauseTime: number) {
+    hue = 255
+    brightness = 255
+    light.defaultStrip().setPixelColor(0, 0xFF0000)
+    light.defaultStrip().setPixelColor(9, 0xFF0000)
+    showStripPixel(0, 0)
+    printStrip(light.defaultStrip())
+    pause(pauseTime)
+    clearStrips(true, true, true)
+    light.defaultStrip().setPixelColor(1, 0xFF0000)
+    light.defaultStrip().setPixelColor(8, 0xFF0000)
+    showStripPixel(1, 1)
+    printStrip(light.defaultStrip())
+    pause(pauseTime)
+    clearStrips(true, true, true)
+    light.defaultStrip().setPixelColor(2, 0xFF0000)
+    light.defaultStrip().setPixelColor(7, 0xFF0000)
+    showStripPixel(2, 2)
+    printStrip(light.defaultStrip())
+    pause(pauseTime)
+    clearStrips(true, true, true)
+    light.defaultStrip().setPixelColor(3, 0xFF0000)
+    light.defaultStrip().setPixelColor(6, 0xFF0000)
+    showStripPixel(3, 3)
+    printStrip(light.defaultStrip())
+    pause(pauseTime)
+    clearStrips(true, true, true)
+    light.defaultStrip().setPixelColor(4, 0xFF0000)
+    light.defaultStrip().setPixelColor(5, 0xFF0000)
+    showStripPixel(0, 0)
+    printStrip(light.defaultStrip())
+    pause(pauseTime)
+    clearStrips(true, true, true)
+    light.defaultStrip().setPixelColor(3, 0xFF0000)
+    light.defaultStrip().setPixelColor(6, 0xFF0000)
+    showStripPixel(1, 1)
+    printStrip(light.defaultStrip())
+    pause(pauseTime)
+    clearStrips(true, true, true)
+    light.defaultStrip().setPixelColor(2, 0xFF0000)
+    light.defaultStrip().setPixelColor(7, 0xFF0000)
+    showStripPixel(2, 2)
+    printStrip(light.defaultStrip())
+    pause(pauseTime)
+    clearStrips(true, true, true)
+    light.defaultStrip().setPixelColor(1, 0xFF0000)
+    light.defaultStrip().setPixelColor(8, 0xFF0000)
+    showStripPixel(3, 3)
+    printStrip(light.defaultStrip())
+    pause(pauseTime)
+    clearStrips(true, true, true)
+}
+
+function clearStrips(right: boolean, left: boolean, ring: boolean) {
+    if (right) {
+        rightStrip.clear()
+    }
+    if (left) {
+        leftStrip.clear()
+    }
+    if (ring) {
+        light.defaultStrip().clear()
+    }
+}
+
+function cycleRainbow() {
+    for (let index = 0; index <= 9; index++) {
+        updateHue()
+        light.defaultStrip().setPixelColor(index, light.hsv(hue, 255, brightness))
+        showStripPixel(-1, index % 4)
+        showStripPixel(index % 4, -1)
+        pause(10)
+    }
+    updateBrightness()
+}
+
+function updateBrightness() {
+    brightness = (brightness + 1) % 255
+    if (brightness <= 100) {
+        brightness = brightness + 100
+    }
+}
+
+function updateHue() {
+    hue = (hue + 1) % 255
+}
+
+function showStripPixel(rightIdx: number, leftIdx: number) {
+    if (rightIdx >= 0) rightStrip.setPixelColor(rightIdx, light.hsv(hue, 255, brightness))
+    if (leftIdx >= 0) leftStrip.setPixelColor(leftIdx, light.hsv(hue, 255, brightness))
+}
+
+let leftStrip: light.NeoPixelStrip = null
+let rightStrip: light.NeoPixelStrip = null
+let brightness = 0
+let hue = 0
+
+rightStrip = light.createStrip(pins.A1, 4)
+leftStrip = light.createStrip(pins.A6, 4)
+hue = 255
+brightness = 100
+
+for (let index = 0; index <= 9; index++) {
+    light.defaultStrip().setPixelColor(index, light.hsv(hue, 255, brightness))
+}
+
+forever(function () {
+    if (input.switchRight()) {
+        cycleRainbow()
+    } else {
+        cylon(300)
+    }
+})
